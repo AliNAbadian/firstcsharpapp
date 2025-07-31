0e53a80616650fd550811ae118783b9e8ea79b8f
AliNAbadian <nagshrizali@gmail.com>
2025-07-31 17:49:55 +0330

    My First C# Project TO learn .NET

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to My Terminal App!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine() ?? string.Empty;
+            string[] nameParts = name.Split(' ');
+            int[] nameLengths = new int[nameParts.Length];
+            for (int i = 0; i < nameParts.Length; i++)
+            {
+                nameLengths[i] = nameParts[i].Length;
+                Console.WriteLine($"Part {i}: {nameParts[i]}");
+            }
+            Console.WriteLine($"Hello, {nameParts[0]}!");
+        }
+    }
+}
